ae9d045b91a3e4fc614a1f08d377f0996cc745b7
leonlu1151 <yclu.pcs09g@nctu.edu.tw>
2021-03-28 18:38:04 +0800

    update for the status image and the failing test case

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# NYCU-Software-Testing-2021-Lab3 
+# NYCU-Software-Testing-2021-Lab3
+[![Build Status](https://travis-ci.com/leonlu1151/509557008.svg?branch=main)](https://travis-ci.com/leonlu1151/509557008)
--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -14,7 +14,7 @@ public class PriorityQueueTest {
     static Stream<Arguments> stringIntAndListProvider() {
         return Stream.of(
                 arguments(new int[]{3, 1, 2}, new int[]{1, 2, 3}),
-                arguments(new int[]{-3, -1, -2, 5}, new int[]{-3, -2, -1, 5}),
+                arguments(new int[]{-3, -1, -2, 5}, new int[]{-2, -2, -1, 5}),
                 arguments(new int[]{3, -2, -5, -1, 2}, new int[]{-5, -2, -1, 2, 3}),
                 arguments(new int[]{-3, 1, 11, 0, 9, 3}, new int[]{-3, 0, 1, 3, 9, 11}),
                 arguments(new int[]{3, 7, 2, -1, -2}, new int[]{-2, -1, 2, 3, 7})
